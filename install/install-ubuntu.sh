b1984e713f7f635612f11abb555b48e0693eef42
morph1904 <morph1904@gmail.com>
2019-05-26 21:46:40 +0100

    Changed to uwsgi

--- install/install-ubuntu.sh
+++ install/install-ubuntu.sh
@@ -8,7 +8,7 @@ LIME_YELLOW=$(tput setaf 190)
 # do not change, these are hardcoded elsewhere
 APPS_DIR=/apps
 TYGER_ROOT=$APPS_DIR/Tyger2
-TYGER_DATA=$TYGER_DIR/data
+TYGER_DATA=$TYGER_ROOT/data
 TYGER_LOGS=$TYGER_ROOT/logs
 
 if [ "$(whoami)" != 'root' ]; then
@@ -20,7 +20,7 @@ if [ "$(whoami)" != 'root' ]; then
   exit 1
 fi
 
-printf "${GREEN}You are about to install Tyger2 BETA.${NORMAL}\n"
+printf "${GREEN}You are about to install TygerCaddy.${NORMAL}\n"
 read -p "${LIME_YELLOW}Proceed? (y/N)${NORMAL} " -r
 printf "\n"
 if [[ ! $REPLY =~ ^[Yy*] ]]
@@ -39,8 +39,7 @@ apt-get update && apt-get -y upgrade && apt-get -y install --no-install-recommen
   libssl-dev \
   libffi-dev \
   git \
-  curl \
-  gunicorn3
+  curl
 
 printf "${GREEN}Cloning repository...${NORMAL}\n"
 mkdir $APPS_DIR
@@ -49,56 +48,53 @@ cd $APPS_DIR
 git clone https://github.com/morph1904/Tyger2.git
 
 mkdir -p $TYGER_DATA \
-         $TYGER_LOGS
+         $TYGER_DATA/logs
 
 touch $TYGER_DATA/caddyfile.conf
 touch $TYGER_LOGS/caddy.txt
-touch $TYGER_LOGS/gunicorn.txt
+touch $TYGER_LOGS/uwsgi.txt
 
 printf "${GREEN}Installing Caddy...${NORMAL}\n"
-curl https://getcaddy.com | bash -s personal tls.dns.cloudflare,tls.dns.godaddy,tls.dns.googlecloud,tls.dns.linode,tls.dns.namecheap
+curl https://getcaddy.com | bash -s personal hook.service
 
 printf "${GREEN}Creating folders, moving files, setting permissions...${NORMAL}\n"
+mkdir -p /etc/caddy \
+         /etc/ssl/caddy
 
 cp $TYGER_ROOT/install/caddy.service        /etc/systemd/system/caddy.service
-cp $TYGER_ROOT/install/gunicorn.service        /etc/systemd/system/gunicorn.service
-cp $TYGER_ROOT/install/gunicorn.socket        /etc/systemd/system/gunicorn.socket
-cp $TYGER_ROOT/install/gunicorn.conf        /etc/tmpfiles.d/gunicorn.conf
+cp $TYGER_ROOT/install/uwsgi.service        /etc/systemd/system/uwsgi.service
 cp $TYGER_ROOT/install/caddy-reload.path    /etc/systemd/system/caddy-reload.path
 cp $TYGER_ROOT/install/caddy-reload.service /etc/systemd/system/caddy-reload.service
 
-chown -R www-data:root /apps/Tyger2/certs
+chown -R www-data:root /etc/caddy \
+                       /etc/ssl/caddy
 chown -R www-data:www-data $TYGER_ROOT
 chown root:root /etc/systemd/system/caddy.service \
-                /etc/systemd/system/gunicorn.service \
-                /etc/systemd/system/gunicorn.socket \
-                /etc/tmpfiles.d/gunicorn.conf \
+                /etc/systemd/system/uwsgi.service \
                 /etc/systemd/system/caddy-reload.path \
                 /etc/systemd/system/caddy-reload.service \
                 /usr/local/bin/caddy
 
-chmod -R 700 /apps/Tyger2/certs
+chmod -R 700 /etc/ssl/caddy
 chmod -R 755 $TYGER_ROOT \
              /usr/local/bin/caddy \
              /etc/systemd/system/caddy.service \
              /etc/systemd/system/caddy-reload.path \
              /etc/systemd/system/caddy-reload.service \
-             /etc/systemd/system/gunicorn.service \
-                /etc/systemd/system/gunicorn.socket \
-                /etc/tmpfiles.d/gunicorn.conf 
+             /etc/systemd/system/uwsgi.service
 
 setcap 'cap_net_bind_service=+eip' /usr/local/bin/caddy
 
 printf "${GREEN}Setting up services to run on boot...${NORMAL}\n"
 systemctl daemon-reload
 systemctl enable caddy.service
-systemctl enable gunicorn.socket
+systemctl enable uwsgi.service
 
 printf "${GREEN}Setting up initial install...${NORMAL}\n"
-pip3 install -r $TYGER_ROOT/newrequirements.txt
+pip3 install -r $TYGER_DIR/newrequirements.txt
 
 printf "${GREEN}Starting TygerCaddy... Almost there!${NORMAL}\n"
-systemctl start gunicorn
+systemctl start uwsgi
 systemctl start caddy
 
 printf "${GREEN}Install complete! Enter the server IP in your chosen browser complete the install wizard.${NORMAL}\n"
