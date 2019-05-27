2ecfd35b4682611ac861b62ad9e2a4ecd6f00b35
morph1904 <morph1904@gmail.com>
2019-05-27 14:26:41 +0100

    Update Settings

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -46,14 +46,14 @@ Vue.use(VueAuth, {
     http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
     router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
     loginData: {
-        url: 'api-token-auth/',
+        url: window.settings.root + 'api-token-auth/',
         method: 'POST',
         redirect: 'home',
         fetchUser: false
     },
     tokenDefaultName: 'token',
     refreshData: {
-        url: 'api-token-refresh/',
+        url: window.settings.root + 'api-token-refresh/',
         method: 'POST',
         enabled: true,
         interval: 2,
