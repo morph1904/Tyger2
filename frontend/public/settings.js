ccfca21529fcfc853121207b956c627261483ecc
morph1904 <morph1904@gmail.com>
2019-05-27 15:06:41 +0100

    hange Settings

--- frontend/public/settings.js
+++ frontend/public/settings.js
@@ -5,6 +5,7 @@ export const Settings = {
     root: root,
     API: {
         LOGIN: root + 'api-token-auth/',
+        REFRESH: root + 'api-token-refresh/',
         REGISTER: root + 'user/register',
       }
 };
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -46,14 +46,14 @@ Vue.use(VueAuth, {
     http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
     router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
     loginData: {
-        url: APIAUTH,
+        url: Settings.API.LOGIN,
         method: 'POST',
         redirect: 'home',
         fetchUser: false
     },
     tokenDefaultName: 'token',
     refreshData: {
-        url: APIREFRESH,
+        url: Settings.API.REFRESH,
         method: 'POST',
         enabled: true,
         interval: 2,
