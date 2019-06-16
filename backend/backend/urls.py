cbb46992e345f09e9fd85d3ecfcec050b6c8d95f
morph1904 <morph1904@gmail.com>
2019-06-16 17:28:26 +0100

    added generate url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -98,5 +98,5 @@ urlpatterns = [
     path('install/', include('install.urls')),
     path('stats/', include('stats.urls')),
     path('', include(router.urls)),
-    path('generate/', caddy.build_caddyfile.as_view())
+    path('generate/', caddy.build_caddyfile(request))
 ]
