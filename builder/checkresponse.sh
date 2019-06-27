02538525d7a036b87cb31434c6839137e655c14f
morph1904 <morph1904@gmail.com>
2019-06-27 11:17:10 +0100

    updated check script

--- builder/checkresponse.sh
+++ builder/checkresponse.sh
@@ -2,7 +2,7 @@
 sleep 2
 
 #check to see if local webserver is serving pages
-response=`curl -s -o /dev/null -I -w "%{http_code}" http://127.0.0.1/$1`
+response=`curl -s -o /dev/null -I -w "%{http_code}" http://127.0.0.1:9091`
 
 #look for a response code of 301 or 302
 if [ $response -eq "301" ] || [ $response -eq "302" ]; then
